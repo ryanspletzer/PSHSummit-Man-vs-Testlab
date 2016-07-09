3c8d04a91712b4a20f4a42c35baabc4510a5ede8
Ryan Spletzer <ryanspletzer@gmail.com>
2016-07-09 14:04:38 -0400

    Tweaks

--- PSHSummit.ps1
+++ PSHSummit.ps1
@@ -3,7 +3,7 @@ Push-Location '~\OneDrive\Documents\PSHSummit\Man vs Test Lab'
 Import-Module Lability -Force
 Get-Command -Module Lability
 Get-LabHostDefaults
-Set-LabHostDefaults -ConfigurationPath D:\TestLab\Configurations -DifferencingVhdPath 'D:\TestLab\VM Disks' -HotfixPath D:\TestLab\Hotfixes -IsoPath D:\TestLab\ISOs -ParentVhdPath 'D:\TestLab\Parent Disks' -ResourcePath D:\TestLab\Resources
+Set-LabHostDefaults -ConfigurationPath D:\TestLab\Configurations -UpdatePath D:\TestLab\Updates -DifferencingVhdPath 'D:\TestLab\VM Disks' -HotfixPath D:\TestLab\Hotfixes -IsoPath D:\TestLab\ISOs -ParentVhdPath 'C:\TestLab\Parent Disks' -ResourcePath D:\TestLab\Resources
 
 <#
     Test host configuration and start configuration if necessary (Start-LabConfiguration calls Test-LabConfiguration anyway!) #>
@@ -12,20 +12,20 @@ Start-LabHostConfiguration -Verbose
 
 <#
     Import configuration into session and generate the MOFs #>
-. .\TLGBaseConfiguration.ps1
-TLGBaseConfiguration -OutputPath D:\TestLab\Configurations -ConfigurationData .\TLGVirtualEngineLab.psd1
+. .\TestLabGuide.ps1
+TestLabGuide -OutputPath D:\TestLab\Configurations -ConfigurationData .\TestLabGuide.psd1
 
 <#
     Set the lab VM defaults, create the lab and start the VMs #>
 Get-LabVMDefaults
-Set-LabVMDefaults -SystemLocale en-US -InputLocale 0409:00000409 -UserLocale en-US -RegisteredOrganization 'Contoso' -StartupMemory 1.5GB
-Start-LabConfiguration -ConfigurationData .\TLGVirtualEngineLab.psd1 -Path D:\TestLab\Configurations -Verbose
+Set-LabVMDefaults -SystemLocale en-US -InputLocale 0409:00000409 -UserLocale en-US -RegisteredOrganization 'Contoso' -StartupMemory 2GB
+Start-LabConfiguration -ConfigurationData .\TestLabGuide.psd1 -Path D:\TestLab\Configurations -Verbose -Force
 
 ## ADD ADDITIONAL NIC
-Add-VMNetworkAdapter -VMName EDGE1 -SwitchName Internet
+#Add-VMNetworkAdapter -VMName EDGE1 -SwitchName Internet
 
 <# Start Lab #>
-Start-Lab -ConfigurationData .\TLGVirtualEngineLab.psd1 -Verbose
+Start-Lab -ConfigurationData .\TestLabGuide.psd1 -Verbose
 
 <#
     List available 'included' media #>
@@ -36,12 +36,12 @@ Get-LabMedia | Select Id, Description, Filename
 Get-LabImage | Select Id, ImagePath
 
 <# Stop Lab #>
-Stop-Lab -ConfigurationData .\TLGVirtualEngineLab.psd1 -Verbose
+Stop-Lab -ConfigurationData .\TestLabGuide.psd1 -Verbose
 
 
 
 
 <# Reset lab #>
 Import-Module Lability -Force
-Remove-LabConfiguration -ConfigurationData .\TLGVirtualEngineLab.psd1 -RemoveSwitch -Verbose
+Remove-LabConfiguration -ConfigurationData .\TestLabGuide.psd1 -Verbose
 Remove-Item -Path $env:ALLUSERSPROFILE\Lability -Force -Recurse
